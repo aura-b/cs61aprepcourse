e06f657872276152cdf5716993f2aa0c5659ad04
Aura Barrera <aura.barrera7@gmail.com>
2016-12-22 14:57:12 -0800

    Add the things to try chap 2

---
+++ calc.rb
@@ -0,0 +1,20 @@
+#puts 1+2
+#puts 1.0 + 2.0
+#puts 2.0 * 3.0
+#puts 5.0 - 8.0
+#puts 9.0 / 2.0
+#puts 1+2
+#puts 2*3
+#puts 5-8
+#puts 9/2
+#puts 5 * (12-8) + -15
+#puts 98 + (59872 / (13*8) * -51)
+
+#hours in a year
+puts 24 * 365
+#minutes in a decade
+puts 60 * 24 * 365.25 * 10
+#my age in seconds
+puts ((((365*24)+6)*60)*60)*19
+#author's age
+puts 1160000000/31557600
